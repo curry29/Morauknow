107a5e6e93c3c116fd13c2d41abef560dfe3bcb3
youtanagai <youtanagai@gmail.com>
2016-06-11 18:49:35 +0900

    ホーム画面を変更し、イベントリストに遷移出来るようにしました。

--- app/src/main/java/funhacks/curry29/morauknow/HomeActivity.java
+++ app/src/main/java/funhacks/curry29/morauknow/HomeActivity.java
@@ -1,7 +1,9 @@
 package funhacks.curry29.morauknow;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,13 +12,33 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        View.OnClickListener eventlist_button_clickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                home2eventlist();
+            }
+        };
+        findViewById(R.id.eventlist_button).setOnClickListener(eventlist_button_clickListener);
         /*
-        ImageButton eventlist_button = (ImageButton) this.findViewById(R.id.imageButton);
-        eventlist_button.setImageResource(R.drawable.sample);
-
-
-        ImageButton areamap_button = (ImageButton) this.findViewById(R.id.imageButton2);
-        areamap_button.setImageResource(R.drawable.sample);
+        View.OnClickListener areamap_button_clickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                home2areamap();
+            }
+        };
+        findViewById(R.id.areamap_button).setOnClickListener(areamap_button_clickListener);
         */
+
+
     }
+    private void home2eventlist(){
+        Intent intent = new Intent(getApplication(), EventList.class);
+        startActivity(intent);
+    }
+/*
+    private void home2areamap(){
+        Intent intent = new Intent(getApplication(), EventList.class);
+        startActivity(intent);
+    }
+    */
 }
