3cd8f2665f6e1adff3fa3a32d5a946c76b82170a
youtanagai <youtanagai@gmail.com>
2016-06-11 19:25:33 +0900

    ホームにタイトルをつけて、ボタンをイメージにしました。

--- app/src/main/java/funhacks/curry29/morauknow/EventDtails.java
+++ app/src/main/java/funhacks/curry29/morauknow/EventDtails.java
@@ -1,7 +1,7 @@
 package funhacks.curry29.morauknow;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class EventDtails extends AppCompatActivity {
 
--- app/src/main/java/funhacks/curry29/morauknow/HomeActivity.java
+++ app/src/main/java/funhacks/curry29/morauknow/HomeActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
