96b66d4daa2028e8d443e7b085d07f58feec9274
youtanagai <youtanagai@gmail.com>
2016-06-11 17:50:34 +0900

    hoem画面を作成しました。map画面を削除しました。

---
+++ app/src/main/java/funhacks/curry29/morauknow/HomeActivity.java
@@ -0,0 +1,22 @@
+package funhacks.curry29.morauknow;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        /*
+        ImageButton eventlist_button = (ImageButton) this.findViewById(R.id.imageButton);
+        eventlist_button.setImageResource(R.drawable.sample);
+
+
+        ImageButton areamap_button = (ImageButton) this.findViewById(R.id.imageButton2);
+        areamap_button.setImageResource(R.drawable.sample);
+        */
+    }
+}
